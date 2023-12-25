b0bbd1fe591e401518a1c3f04b1ca92872c521a3
dagi3244 <dagitesfud3244@gmail.com>
2023-12-24 16:57:30 -0800

    Amazon Backend deploy

---
+++ index.js
@@ -0,0 +1,30 @@
+// import { initializeApp } from "firebase-admin/app";
+// const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const stripe = require("stripe")(process.env.SECRET_KEY);
+
+const app = express();
+app.use(cors({ origin: true }));
+app.use(express.json());
+app.get("/", (request, response) => response.status(200).send("hello world"));
+app.post("/payments/create", async (request, response) => {
+  const total = request.query.total;
+  console.log("Payment Request Recieved for this amount >>>", total);
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total,
+    currency: "usd",
+  });
+
+  //ok create
+
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+//listens command
+app.listen(3000, () => console.log("Listening on port 3000"));
+// exports.api = functions.https.onRequest(app);
+
+// http://127.0.0.1:5001/clone-49d14/us-central1/api
